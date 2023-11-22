3ae5039e63adfb96708c92ab9583e6b49ec81038
echao <echao8@uwo.ca>
2023-11-21 23:44:35 -0500

    Adding previous list routes

--- server/server.js
+++ server/server.js
@@ -1,1 +1,10 @@
-console.log("a");
+// Initializing express and dotenv
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use('/api/goals', require('./routes/listRoutes'));
+
+app.listen(port, ()=> console.log(`Listening on port ${port}`));
